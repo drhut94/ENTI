3786334f39d79839c5eaab69c89a172c5e5fc3ac
drhut94 <drhut94@gmail.com>
2019-11-25 13:33:11 +0100

    Unrea + 3D

---
+++ Unity/AnimTest/Assets/InputManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+
+    
+    public Vector3 movmentAxis;
+    public bool runInput;
+
+    public void Update()
+    {
+        movmentAxis = new Vector3(Input.GetAxis("Vertical"), 0, Input.GetAxis("Horizontal"));
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            runInput = true;
+        }
+        else
+        {
+            runInput = false;
+        }
+    }
+}
---
+++ Unity/AnimTest/Assets/PlaerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaerController : MonoBehaviour
+{
+    #region Managers
+    CharacterController controller;
+    InputManager inputManager;
+    #endregion
+
+    #region MovmentVariables
+
+    [Header("Movment")]
+    public float walkingSpeed;
+    public float runningSpeed;
+    public float acceleration;
+
+    public float velocity;
+
+    #endregion
+
+    public void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        inputManager = GetComponent<InputManager>();
+    }
+
+    public void FixedUpdate()
+    {
+        Move();
+
+    }
+
+    private void Move()
+    {
+        if (inputManager.runInput)
+            velocity = Mathf.Lerp(velocity, runningSpeed, acceleration);
+        else
+            velocity = Mathf.Lerp(velocity, walkingSpeed, acceleration);
+
+
+        controller.Move(inputManager.movmentAxis * velocity);
+        controller.
+    }
+}
